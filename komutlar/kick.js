497f2dd02ca3f964b8254d69d9fabbf241aeabe6
zraeixd <93719188+zraeixd@users.noreply.github.com>
2021-11-17 10:16:03 +0300

    Update kick.js

--- komutlar/kick.js
+++ komutlar/kick.js
@@ -4,24 +4,24 @@ const ayarlar = require('../ayarlar.json')
 exports.run = async (bot, message, args) => {
 var prefix = ayarlar.prefix;             
     
-  if (!message.member.permissions.has("KICK_MEMBERS")) return message.channel.send(`Bu komutu kullanabilmek için **Üyeleri At** iznine sahip olmalısın!`);
+  if (!message.member.permissions.has("KICK_MEMBERS")) return message.channel.send(`Bu komutu kullanabilmek için gerekli izine sahip değilsiniz! [ Gereken Yetki -> **Üyeleri At** ]`);
   
 	let user = message.mentions.users.first() || message.client.users.cache.get(args[0]) || message.client.users.cache.find(m => m.username === args.slice(0).join(" ")) || message.author;
   let reason = args.slice(1).join(' ');
   
-  if (message.mentions.users.size < 1) return message.channel.send(`Sunucudan atmam için istediğiniz kullanıcıyı etiketlemelisiniz; \`${prefix}at @Gnarge Uyarı\` `);
+  if (message.mentions.users.size < 1) return message.channel.send(`Sunucudan atmam için istediğiniz kullanıcıyı etiketlemelisiniz; \`${prefix}at @Zraei Uyarı\` `);
   if (user.id === message.author.id) return message.channel.send('Kendini atamazsın.');
-if (user.position > message.member.roles.highest.position) return message.channel.send(`Bu kullanıcının senin rollerinden/rolünden daha yüksek rolleri/rolü var.`);
+if (user.position > message.member.roles.highest.position) return message.channel.send(`Bu kullanıcının yetkisi senin yetkinden yüksek olduğu için işlem yapamıyorum.`);
 			    if (!reason) reason = 'Belirtilmemiş.'
-    if (!user) return message.channel.send(`Etiketlediğin kullanıcıyı sunucuda bulamadım.`)
+    if (!user) return message.channel.send(`Etiketlediğiniz kullanıcıyı sunucumuzda bulamadım.`)
     let member = message.guild.member(user)
-    if (!member) return message.channel.send(`Etiketlediğin kullanıcıyı sunucuda bulamadım.`)
+    if (!member) return message.channel.send(`Etiketlediğiniz kullanıcıyı sunucumuzda bulamadım.`)
 
- if (!message.guild.member(user).bannable) return message.channel.send(`Bu kişiyi sunucudan atamıyorum çünkü \`benden daha yüksek bir role sahip\` ya da \`bana gerekli yetkileri vermedin\`.`);
+ if (!message.guild.member(user).bannable) return message.channel.send(`Bu kişiyi sunucumuzdan atamıyorum çünkü benden yüksek yetkisi var veya benim gerekli yetkilerimi vermediniz.`);
 
-   if (!message.guild.member(user).bannable) return message.channel.send('Sunucudaki yetkilileri atamam!');
+   if (!message.guild.member(user).bannable) return message.channel.send('Sunucumuzda ki yetkilileri atamam!');
     message.guild.member(user).kick(reason);
-message.channel.send(`<@${user.id}> **Adlı kullanıcı sunucudan atıldı!** **Sebep: \`${reason}\``)
+message.channel.send(`<@${user.id}> **Adlı kullanıcı sunucumuzdan atıldı!** **Sebep: \`${reason}\``)
 
 
 };
@@ -34,7 +34,7 @@ exports.conf = {
 
 exports.help = {
   name: 'kick',
-  description: 'Belirttiğiniz kişiyi sunucudan atar.',
+  description: 'Belirttiğiniz kişiyi sunucumuzdan atar.',
   usage: 'kick <@kullanıcı> <sebep>',
  
 };
